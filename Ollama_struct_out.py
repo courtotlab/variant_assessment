6e804fa38c3bb340576f747c82b0c3c7736cf35e
Adnan Ahmed <adnanahmed701@gmail.com>
2025-06-10 14:31:23 -0400

    changed struct to include type

--- Ollama_struct_out.py
+++ Ollama_struct_out.py
@@ -2,26 +2,51 @@ from langchain_ollama import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
 import json
 
-def call_ollama_struct_out(system_msg:str, query:str, model_used:str)->dict:
+def call_ollama_struct_out(system_msg: str, query: str, model_used: str) -> dict:
     llm = ChatOllama(model=model_used, temperature=0)
 
-    json_schema = {}
-    json_schema["title"] = "report"
-    json_schema["description"] = "Information of the molecular report"
-    json_schema["type"] = "object"
-    json_schema["properties"] = {}
+    json_schema = {
+        "title": "report",
+        "description": "Information of the molecular report",
+        "type": "object",
+        "properties": {}
+    }
 
-    field_definitions = system_msg.split("## JSON schema")[1].split("##")[0].split("\n")
+    try:
+        # Extract JSON schema block
+        field_definitions = system_msg.split("## JSON schema")[1].split("##")[0].split("\n")
+    except IndexError:
+        raise ValueError("Prompt is missing the '## JSON schema' section.")
 
     for field in field_definitions:
-        if len(field) > 0 and field[0].isdigit():
+        if len(field.strip()) > 0 and field.strip()[0].isdigit():
             field_parts = field.split("/")
-            field_name_full = field_parts[0]
-            field_description = field_parts[1]
+            if len(field_parts) < 2:
+                continue  # Skip invalid lines
 
-            field_name = field_name_full[field_name_full.find('"') + 1 : len(field_name_full)-1]
+            field_name_full = field_parts[0].strip()
+            field_description = field_parts[1].strip()
 
-            json_schema["properties"][field_name] = {"description":field_description.strip()}
+            # Extract field name from quotes
+            field_name = field_name_full[field_name_full.find('"') + 1 : field_name_full.rfind('"')]
+
+            # Guess type from description
+            desc_lower = field_description.lower()
+            if "list" in desc_lower:
+                field_type = "array"
+            elif "dictionary" in desc_lower or "dict" in desc_lower:
+                field_type = "object"
+            elif "int" in desc_lower or "number" in desc_lower:
+                field_type = "integer"
+            else:
+                field_type = "string"
+
+            json_schema["properties"][field_name] = {
+                "description": field_description,
+                "type": field_type
+            }
+
+    print(json_schema)
 
     prompt = ChatPromptTemplate.from_messages(
         [
@@ -31,9 +56,7 @@ def call_ollama_struct_out(system_msg:str, query:str, model_used:str)->dict:
     )
 
     structured_llm = llm.with_structured_output(json_schema)
-
     chain = prompt | structured_llm
 
-    result = chain.invoke({"query":query})
-
+    result = chain.invoke({"query": query})
     return result
