811a63ca9f005188d09cfb730a8ebe52fc667ad1
Andres Melani <af.melani3365@uniandes.edu.co>
2026-03-26 14:38:40 -0400

    Renamed two-pass prompts folders for clarity. Updated prompt paths in constants

--- pipeline/constants.py
+++ pipeline/constants.py
@@ -18,16 +18,16 @@ LLAMA_PROMPTS_PATH = {"1_pass":{"COT":os.path.join(PROMPTS_PATH, "1_pass", "llam
                               "few_shot":os.path.join(PROMPTS_PATH, "1_pass", "llama_prompts", "llama_prompt_few_shot.txt"),
                               "one_shot":os.path.join(PROMPTS_PATH, "1_pass", "llama_prompts", "llama_prompt_one_shot.txt"),
                               "zero_shot":os.path.join(PROMPTS_PATH, "1_pass", "llama_prompts", "prompt_with_quote_llama.txt")},
-                    "2_pass":{"COT":(os.path.join(PROMPTS_PATH, "2_pass", "llama_prompts_first_pass", "COT.txt"),
-                                     os.path.join(PROMPTS_PATH, "2_pass", "llama_prompts_second_pass", "COT.txt")),
-                            "few_shot_COT":(os.path.join(PROMPTS_PATH, "2_pass", "llama_prompts_first_pass", "few_shot_COT.txt"),
-                                     os.path.join(PROMPTS_PATH, "2_pass", "llama_prompts_second_pass", "few_shot_COT.txt")),
-                            "few_shot":(os.path.join(PROMPTS_PATH, "2_pass", "llama_prompts_first_pass", "few_shot.txt"),
-                                     os.path.join(PROMPTS_PATH, "2_pass", "llama_prompts_second_pass", "few_shot.txt")),
-                            "one_shot":(os.path.join(PROMPTS_PATH, "2_pass", "llama_prompts_first_pass", "one_shot.txt"),
-                                     os.path.join(PROMPTS_PATH, "2_pass", "llama_prompts_second_pass", "one_shot.txt")),
-                            "zero_shot":(os.path.join(PROMPTS_PATH, "2_pass", "llama_prompts_first_pass", "zero_shot.txt"),
-                                     os.path.join(PROMPTS_PATH, "2_pass", "llama_prompts_second_pass", "zero_shot.txt"))}} 
+                    "2_pass":{"COT":(os.path.join(PROMPTS_PATH, "2_pass", "first_pass", "COT.txt"),
+                                     os.path.join(PROMPTS_PATH, "2_pass", "second_pass", "COT.txt")),
+                            "few_shot_COT":(os.path.join(PROMPTS_PATH, "2_pass", "first_pass", "few_shot_COT_extended.txt"),
+                                     os.path.join(PROMPTS_PATH, "2_pass", "second_pass", "few_shot_COT_extended.txt")),
+                            "few_shot":(os.path.join(PROMPTS_PATH, "2_pass", "first_pass", "few_shot.txt"),
+                                     os.path.join(PROMPTS_PATH, "2_pass", "second_pass", "few_shot.txt")),
+                            "one_shot":(os.path.join(PROMPTS_PATH, "2_pass", "first_pass", "one_shot.txt"),
+                                     os.path.join(PROMPTS_PATH, "2_pass", "second_pass", "one_shot.txt")),
+                            "zero_shot":(os.path.join(PROMPTS_PATH, "2_pass", "first_pass", "zero_shot.txt"),
+                                     os.path.join(PROMPTS_PATH, "2_pass", "second_pass", "zero_shot.txt"))}}
 
 # Input constants -----------------------------------------------
 
